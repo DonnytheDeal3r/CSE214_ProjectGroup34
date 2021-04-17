e36117aaa75ae0ad53c6af62ec20501d394bd6d4
Nicol Visser <nicolvisser@yahoo.com>
2021-04-17 12:15:31 +0200

    matrix

--- Game.java
+++ Game.java
@@ -4,5 +4,14 @@ public class Game {
       System.out.println("Hey nicol!");
       System.out.println("hi donny!");
       System.out.println();
-     }
+      for (int i = 0; i < 8; i++)
+      {
+        for (int j = 0; j < 8; j++)
+        {
+            System.out.print((i+j) + "\t\t");
+        }
+        System.out.println();
+      }
+        
+    }
 }
