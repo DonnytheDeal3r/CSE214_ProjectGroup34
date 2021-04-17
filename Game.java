1baf8fff106d11307fbbde8d0bb8c6ac0ca255fa
Nicol Visser <nicolvisser@yahoo.com>
2021-04-17 12:11:40 +0200

    fixed syntax error

--- Game.java
+++ Game.java
@@ -1,7 +1,7 @@
 public class Game {
      public static void main(String[] args) {
       System.out.println("hello world");
-      System.out.println("Hey nicol!")
+      System.out.println("Hey nicol!");
       System.out.println("hi donny!");
       System.out.println();
      }
