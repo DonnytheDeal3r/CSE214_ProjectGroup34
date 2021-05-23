3d4f93a53da996050432930ef362961108846fb2
DonnytheDeal3r <82703308+DonnytheDeal3r@users.noreply.github.com>
2021-05-23 23:49:30 +0200

    Add files via upload

---
+++ src.new/Object2D.java
@@ -0,0 +1,116 @@
+/*Created by:H.C. de Beer
+ *           J.J. Mocke
+ *           
+ *            
+ */
+public class Object2D {
+
+    double x;                                     //x-coord
+    double y;                                     //y-coord
+    double vx;                                    //x-velocity
+    double vy;                                    //y-velocity
+    double ax;                                    //x-accelaration
+    double ay;                                    //y-accelaration
+    double angleref;                              //angle of object
+
+    public Object2D(double x0, double y0, double angle, double vxin) {
+        x = x0;                                   //intialization
+        y = y0;                                   //    "
+        vx = vxin;                                   //    "
+        vy = 0;                                   //    "
+        ax = 0;                                   //    "
+        ay = 0;                                   //    "
+        angleref = angle;                         //    "
+    }
+//------------------------(updates)-----------------------------------------------------
+    
+
+    //update enemy
+    public void update() {
+        double dt = 1;                            //time
+        vx += ax * dt;                            //new vx
+        vy += ay * dt;                            //new vy
+        x += vx * dt + 0.5 * ax * dt * dt;        //new x
+        y += vy * dt + 0.5 * ay * dt * dt;        //new y
+        
+        
+      if(x>=780){
+        vx = -vx;
+        y -= 150;
+      }
+      if(y>=800){
+        vy = -vy;
+      }
+      if(x<=20){
+        vx = -vx;
+        y -= 150;
+      }
+      if(y<=0){
+        vy = -vy;
+      }
+        
+    }
+    
+    
+    //update shooter
+    public void update1() {
+        double dt = 1;                            //time
+        vx += ax * dt;                            //new vx
+        vy += ay * dt;                            //new vy
+        x += vx * dt + 0.5 * ax * dt * dt;        //new x
+        y += vy * dt + 0.5 * ay * dt * dt;        //new y
+        
+
+      if(x>=780){
+        vx = -vx;
+       
+      }
+      if(y>=800){
+        vy = -vy;
+      }
+      if(x<=20){
+        vx = -vx;
+       
+      }
+      if(y<=0){
+        vy = -vy;
+      }
+        
+    }
+    
+    
+    //update Missle  
+    public void update2() {
+        double dt = 1;                          //time        
+        vx += ax * dt;                          //new vx
+        vy += ay * dt;                          //new vy
+        x += vx * dt + 0.5 * ax * dt * dt;      //new x
+        y += vy * dt + 0.5 * ay * dt * dt;      //new y
+    }
+    
+    
+//------------(getters and setters)------------------------------------------
+    
+    //return of y-coord
+    public double getY(){
+    return y;
+    }
+    
+    //return of x-coord
+    public double getX(){
+    return x;
+    }
+    
+    //return of angle
+    public double reAng(){
+     double angle1 = angleref;
+     return angle1;
+    }
+    
+    //changevx
+     public void changeVx(double vX){
+     vx = vX;
+    }
+     
+//------------------------(end)--------------------------------------------------
+}
